f3da07f6e5e6923356aa8bf53a270b8da0b3cd25
masato-occ <masato@occ.co.jp>
2021-05-11 14:03:33 +0900

    プロジェクト ファイルを追加します。

---
+++ trycommit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace trycommit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
